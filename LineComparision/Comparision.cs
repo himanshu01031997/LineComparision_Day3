f00c9c2cd050282267c02c400cba66b2bd8651b8
himanshu01031997 <hs010397@gmail.com>
2022-10-06 21:04:53 +0530

    add:added java equals to method

--- LineComparision/Comparision.cs
+++ LineComparision/Comparision.cs
@@ -10,17 +10,18 @@ namespace LineComparision
     {
           int x1, y1, x2, y2;
         
-        public  void value(int x1,int y1,int x2,int y2)
+        public Comparision(int x1,int y1,int x2,int y2)//constructor
         {
             this.x1 = x1;
             this.y1 = y1;
             this.x2 = x2;
             this.y2 = y2;
         }
-        public  void calculate()
+        public  double calculate()
         {
             double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
-            Console.WriteLine("length of the line is {0}",length);
+         //   Console.WriteLine("length of the line is {0}",length);
+            return length;
 
         }
     }
