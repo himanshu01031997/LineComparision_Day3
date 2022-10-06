070af8abf1710e56cc39738428441fcfe0efd043
himanshu01031997 <hs010397@gmail.com>
2022-10-06 16:36:29 +0530

    add:welcome message

---
+++ LineComparision/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparision
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to line comparision Problem");
+        }
+    }
+}
